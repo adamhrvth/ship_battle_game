2903780a58391ebae4901972ca13c5f92da0506b
Adam Horvath <horadam0207@gmail.com>
2023-02-19 18:09:21 +0100

    fix: renamed property/resources designer

--- shipbattle_game/Properties/Resources.Designer.cs
+++ shipbattle_game/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace csapda.Properties {
+namespace shipbattle_game.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace csapda.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("trap.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("shipbattle_game.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
